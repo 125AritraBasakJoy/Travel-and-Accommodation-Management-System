9c3f5646506720873a960849a243d568c91a8849
Aritra Basak Joy <basakjoy125@gmail.com>
2025-01-19 23:27:35 +0600

    Update flightView.php

--- app/views/flight.view.php
+++ app/views/flight.view.php
@@ -13,6 +13,7 @@ $Flight = new FlightModel();
 $Flights = $Flight->getAllFlights(); // Fetch data from the database
 ?>
 
+
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="-my-8">
